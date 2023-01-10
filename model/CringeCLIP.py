49082fbe90c99e22d52632c99e55919eed467df8
pizzabug <pizzahutladybug@gmail.com>
2023-01-11 00:01:40 +1030

    feat: Begin implement CLIP model. [#7]

--- model/CringeCLIP.py
+++ model/CringeCLIP.py
@@ -1,25 +1,55 @@
+import open_clip
 import pytorch_lightning as pl
 import torch
-import torch.nn as nn
 
-from torch import Tensor
 from torch.nn import functional as F
-from torch.utils.data import DataLoader
-from torch.utils.data import random_split
-
-from model.unet.unet import UNet
 
 
-class CringeCLIPModel(pl.LightningDataModule):
+class CringeCLIPModel(pl.LightningModule):
     """
         CringeCLIP
     """
 
-    def __init__(self, dimensions=[
-        32, 64, 128, 256
-    ], hparams=None, has_cross_attention=False, img_dim=512):
+    def __init__(self, model_type="RN50", hparams=None, has_cross_attention=False, img_dim=512):
         super().__init__()
 
-        self.img_dim = img_dim
-        self.clip_module = UNet(
-            dimensions=dimensions, hparams=hparams, has_cross_attention=has_cross_attention)
+        self.clip_module, _, self.preprocess = open_clip.create_model_and_transforms("RN50") # type: ignore
+        self.tokenizer = open_clip.get_tokenizer("RN50") # type: ignore
+
+    def forward(self, text = None, image = None):
+        if (text is None) and (image is None):
+            raise ValueError("Must provide either text or image")
+        
+        elif image is not None:
+            x = self.clip_module.encode_image(image) # type: ignore
+            x /= x.norm(dim=-1, keepdim=True)
+            return x
+
+        elif text is not None:
+            x = self.clip_module.encode_text(text) # type: ignore
+            x /= x.norm(dim=-1, keepdim=True)
+            return x
+
+
+    def configure_optimizers(self):
+        """
+            configure_optimizers
+
+            This is the optimizer for the model.
+        """
+        optimizer = torch.optim.Adam(self.parameters(), lr=5e-5)
+        return optimizer
+
+    def training_step(self, train_batch, batch_idx):
+        text, output = train_batch
+        y = self.forward(text=text)
+        loss = F.mse_loss(y, output) # type: ignore
+
+        return loss
+    
+    def validation_step(self, train_batch, batch_idx):
+        text, output = train_batch
+        y = self.forward(text=text)
+        loss = F.mse_loss(y, output) # type: ignore
+
+        return loss
